b8454ae24364814cbf28378af4cd20eac7e59eaf
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-19 18:35:36 +0300

    feat : Add getters and setters for User.

--- src/model/User.java
+++ src/model/User.java
@@ -8,5 +8,42 @@ public class User {
     private String lastName;
     private String email;
     private Date birthDate;
-    
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   Getters                                  */
+    /* -------------------------------------------------------------------------- */
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   Setters                                  */
+    /* -------------------------------------------------------------------------- */
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
