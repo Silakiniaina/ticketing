7e9117968d2629d20fe9783eff79bafce73ed8d0
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-19 19:34:44 +0300

    feat : Change UserController into AuthController.

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -2,14 +2,14 @@ package controller;
 
 import service.UserService;
 
-public class UserController {
+public class AuthController {
 
     private final UserService userService;
 
     /* -------------------------------------------------------------------------- */
     /*                                 Constructor                                */
     /* -------------------------------------------------------------------------- */
-    public UserController(){
+    public AuthController(){
         this.userService = new UserService();
     }
 }
