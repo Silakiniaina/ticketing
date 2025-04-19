7ab8d44d4b16d4188d4ffeabde1b29dfd24e1deb
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-19 19:29:44 +0300

    feat : Add login function in UserService.

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,6 +1,11 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import dao.UserDAO;
+import error.AuthException;
+import model.User;
 
 public class UserService {
     
@@ -11,5 +16,12 @@ public class UserService {
     /* -------------------------------------------------------------------------- */
     public UserService(){
         this.userDAO = new UserDAO();
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*              Service for login a user with email and password              */
+    /* -------------------------------------------------------------------------- */
+    public User login(String email, String password, Connection c) throws AuthException, SQLException{
+        return this.userDAO.login(email, password, c);
     }
 }
