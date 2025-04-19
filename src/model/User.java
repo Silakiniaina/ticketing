cc839377450e4f8f0e42c9f3fe1c5916c34715de
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-19 18:34:01 +0300

    feat : Create model for User.

---
+++ src/model/User.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.sql.Date;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date birthDate;
+    
+}
