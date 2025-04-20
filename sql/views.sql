1ce66a00faa1cbbd7b40f0a01dd9022087877663
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-20 14:32:42 +0300

    feat : Create view v_users.

---
+++ sql/views.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE view v_users AS
+    SELECT 
+        u.*,
+        r."label" AS role
+    FROM _user_ u
+    JOIN user_role ur
+        ON UR.user_id = u.id
+    JOIN _role_ r
+        ON ur.role_id = r.id
+;
