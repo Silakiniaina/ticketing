23132640bce5b5980721654f1a60b5fe232219e7
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-08-11 21:05:27 +0300

    feat : Create endpoint GET /flights

--- src/controller/FlightController.java
+++ src/controller/FlightController.java
@@ -1,9 +1,17 @@
 package controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import exception.DaoException;
 import lombok.Getter;
 import lombok.Setter;
 import mg.dash.mvc.annotation.Auth;
 import mg.dash.mvc.annotation.Controller;
+import mg.dash.mvc.annotation.Get;
+import mg.dash.mvc.annotation.Url;
+import mg.dash.mvc.handler.views.ModelView;
+import model.Flight;
 import service.CityService;
 import service.FlightService;
 import service.PlaneService;
@@ -25,5 +33,32 @@ public class FlightController{
         this.flightService = new FlightService();
         this.cityService = new CityService();
         this.planeService = new PlaneService();
+    }
+
+        /* -------------------------------------------------------------------------- */
+    /*                            Show fligth list page                           */
+    /* -------------------------------------------------------------------------- */
+    @Get
+    @Url("/flights")
+    public ModelView showFlightList(){
+        ModelView mv = new ModelView("/WEB-INF/views/layout/admin-layout.jsp");
+        try {
+            List<Flight> flights = flightService.getAll();
+            if(flights == null){
+                throw new Exception("Flights is null");
+            }
+            String pageTitle  = "List of "+flights.size()+ " flights";
+            mv.addObject("pageTitle", pageTitle);
+            mv.addObject("contentPage", "pages/admin/flight/list.jsp");
+            mv.addObject("flights", flights);
+        } catch (DaoException daoEx) {
+            mv.addObject("error", "Error on DAO while fecthing flight list : "+daoEx.getMessage());
+            daoEx.printStackTrace();
+        } catch (SQLException sqlEx){
+            mv.addObject("error", "Error on SQL while fetching flight list : "+sqlEx.getMessage());
+        } catch (Exception ex){
+            mv.addObject("error", "Unexepted error : "+ex.getMessage());
+        }
+        return mv;
     }
 }
