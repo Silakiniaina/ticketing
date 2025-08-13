a253767dee5712101a6bb343e4af1540e527053b
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-08-13 09:51:18 +0300

    feat : Create dto flightArg for creation flight.

---
+++ src/dto/FlightArg.java
@@ -0,0 +1,33 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mg.dash.mvc.annotation.Numeric;
+import mg.dash.mvc.annotation.Required;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightArg {
+
+    @Required
+    @Numeric
+    private int planeId;
+
+    @Required
+    @Numeric
+    private int departureCityId;
+
+    @Required
+    @Numeric
+    private int arrivalCityId;
+
+    @Required
+    private String departureDatetime;
+
+    @Required
+    private String arrivalDatetime;
+}
