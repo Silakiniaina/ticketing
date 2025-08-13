acb4ad7bf082f16dd6ce8657d91866f4b1a5f989
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-08-13 09:52:55 +0300

    feat : Create function addFlight and injectValues from FlightArg.

--- src/service/FlightService.java
+++ src/service/FlightService.java
@@ -7,11 +7,13 @@ import java.util.List;
 import dao.CityDAO;
 import dao.FlightDAO;
 import dao.PlaneDAO;
+import dto.FlightArg;
 import exception.DaoException;
 import lombok.Getter;
 import lombok.Setter;
 import model.Flight;
 import util.Database;
+import util.DateUtil;
 
 @Getter
 @Setter
@@ -36,5 +38,29 @@ public class FlightService {
     public List<Flight> getAll() throws DaoException,SQLException,Exception{
         Connection c = Database.getActiveConnection();
         return flightDAO.getAll(c);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                Add a flight                                */
+    /* -------------------------------------------------------------------------- */
+    public void addFlight(Flight f) throws DaoException, SQLException, Exception{
+        Connection c = Database.getActiveConnection();
+        flightDAO.insert(c, f);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                  Inject flightArg value on Flight instance                 */
+    /* -------------------------------------------------------------------------- */
+    public Flight injectValues(Flight f, FlightArg args)throws DaoException, SQLException, Exception{
+        Connection c = Database.getActiveConnection();
+        if(f == null){
+            throw new Exception("Cannot inject values on a flight that is null");
+        }
+        f.setPlane(planeDAO.getById(c, args.getPlaneId()));
+        f.setArrivalCity(cityDAO.getById(c, args.getArrivalCityId()));
+        f.setDepartureCity(cityDAO.getById(c, args.getDepartureCityId()));
+        f.setDepartureDatetime(DateUtil.convertStringToTimestamp(args.getDepartureDatetime()));
+        f.setArrivalDatetime(DateUtil.convertStringToTimestamp(args.getDepartureDatetime()));
+        return f;
     }
 }
