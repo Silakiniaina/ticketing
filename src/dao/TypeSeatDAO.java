8083627e3c55bf31ccdf4467bde4aa2ca36288ab
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-08-16 22:24:34 +0300

    feat : Add getAll on typeSeatDAO.

--- src/dao/TypeSeatDAO.java
+++ src/dao/TypeSeatDAO.java
@@ -4,12 +4,44 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import exception.DaoException;
 import model.TypeSeat;
 import util.Database;
 
 public class TypeSeatDAO {
+
+    /* -------------------------------------------------------------------------- */
+    /*                              Get all type seat                             */
+    /* -------------------------------------------------------------------------- */
+    public List<TypeSeat> getAll(Connection c) throws DaoException,SQLException{
+        List<TypeSeat> result = new ArrayList<>();
+        boolean isNewConnection = false;
+        PreparedStatement prstm = null; 
+        ResultSet rs = null; 
+        String query = "SELECT * FROM type_seat";
+        try {
+            if(c == null){
+                c = Database.getActiveConnection(); 
+                isNewConnection = true;
+            }
+            prstm = c.prepareStatement(query);
+            rs = prstm.executeQuery();
+            while(rs.next()){
+                TypeSeat t = new TypeSeat();
+                t.setId(rs.getInt("id"));
+                t.setLabel(rs.getString("label"));
+                result.add(t);
+            }
+            return result;
+        } catch (Exception e) {
+            throw new DaoException(query, e.getMessage());
+        }finally{
+            Database.closeRessources(rs, prstm, c, Boolean.valueOf(isNewConnection));
+        }
+    }
     
     /* -------------------------------------------------------------------------- */
     /*                       Function to get type seat by its id                  */
