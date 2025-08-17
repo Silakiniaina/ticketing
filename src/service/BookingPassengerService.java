50da18e19cc2993985985b86fe791bddf8e1be72
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-08-17 16:15:22 +0300

    feat : Create service for booking passenger with getAllByBookingId and insert methods.

---
+++ src/service/BookingPassengerService.java
@@ -0,0 +1,59 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.BookingDAO;
+import dao.BookingPassengerDAO;
+import dao.TypeSeatDAO;
+import dto.BookingPassengerArg;
+import exception.DaoException;
+import model.BookingPassenger;
+import util.Database;
+import util.DateUtil;
+
+public class BookingPassengerService {
+
+    private final BookingPassengerDAO bookingPassengerDAO;
+    private final BookingDAO bookingDAO;
+    private final TypeSeatDAO typeSeatDAO;
+
+    /* -------------------------------------------------------------------------- */
+    /*                                 Constructor                                */
+    /* -------------------------------------------------------------------------- */
+    public BookingPassengerService() {
+        this.bookingPassengerDAO = new BookingPassengerDAO();
+        this.bookingDAO = new BookingDAO();
+        this.typeSeatDAO = new TypeSeatDAO();
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                        Get all booking passengers by booking ID            */
+    /* -------------------------------------------------------------------------- */
+    public List<BookingPassenger> getAllByBookingId(int bookingId) throws DaoException, SQLException, Exception {
+        Connection c = Database.getActiveConnection();
+        return bookingPassengerDAO.getAllByBookingId(c, bookingId);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                          Add a booking passenger                           */
+    /* -------------------------------------------------------------------------- */
+    public BookingPassenger addBookingPassenger(BookingPassenger bp) throws DaoException, SQLException, Exception {
+        Connection c = Database.getActiveConnection();
+        return bookingPassengerDAO.insert(c, bp);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                               Injects values                               */
+    /* -------------------------------------------------------------------------- */
+    public BookingPassenger injectValues(BookingPassenger bp,BookingPassengerArg bpa)throws DaoException, SQLException, Exception{
+        Connection c = Database.getActiveConnection();
+        if(bp == null){
+            throw new Exception("Cannot inject values on a booking passenger that is null");
+        }
+        bp.setBooking(bookingDAO.getById(c, bpa.getBookingId()));
+        bp.setTypeSeat(typeSeatDAO.getById(c, bpa.getTypeSeatId()));
+        return bp;
+    }
+}
