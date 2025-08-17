2b2fd71cc32f93d52e08dff50b4c74239b0aad8a
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-08-17 16:10:36 +0300

    feat : Add flight type seat price on data.sql.

--- sql/data.sql
+++ sql/data.sql
@@ -9,3 +9,11 @@ INSERT INTO _user_(last_name,email,password,first_name,birth_date) VALUES
 INSERT INTO user_role(role_id, user_id) VALUES 
 (1,1),
 (2,2);
+
+INSERT INTO flight_price_type_seat(flight_id,type_seat_id,price) VALUES
+(1,1,1000),
+(1,2,1500),
+(2,1,1000),
+(2,2,1500),
+(5,1,1000),
+(5,2,1500);
