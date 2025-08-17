05c06e88af3a5ea42f2f29584ccad51a5ad2808a
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-08-17 16:16:47 +0300

    feat : Add function getById on UserDAO.

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -13,25 +13,24 @@ public class UserDAO {
     /* -------------------------------------------------------------------------- */
     /*          Function to login for user with their email and password          */
     /* -------------------------------------------------------------------------- */
-    public User login(String email, String password, Connection c)throws AuthException,SQLException{
+    public User login(String email, String password, Connection c) throws AuthException, SQLException {
         User connected = null; 
         boolean isNewConnection = false;
         PreparedStatement prstm = null; 
         ResultSet rs = null;
         String query = "SELECT * FROM v_users WHERE email = ? AND password = ?";
         try {
-            if(c == null){
+            if (c == null) {
                 c = Database.getActiveConnection();
                 isNewConnection = true;
             }
 
             prstm = c.prepareStatement(query);
-
             prstm.setString(1, email);
             prstm.setString(2, password);
 
             rs = prstm.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 connected = new User();
                 connected.setId(rs.getInt("id"));
                 connected.setLastName(rs.getString("last_name"));
@@ -43,7 +42,41 @@ public class UserDAO {
             return connected;
         } catch (Exception e) {
             throw new AuthException(email, e.getMessage());
-        } finally{
+        } finally {
+            Database.closeRessources(rs, prstm, c, Boolean.valueOf(isNewConnection));
+        }
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                           Get a user by its id                             */
+    /* -------------------------------------------------------------------------- */
+    public User getById(Connection c, int id) throws AuthException, SQLException {
+        User result = null;
+        boolean isNewConnection = false;
+        PreparedStatement prstm = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM v_users WHERE id = ?";
+        try {
+            if (c == null) {
+                c = Database.getActiveConnection();
+                isNewConnection = true;
+            }
+            prstm = c.prepareStatement(query);
+            prstm.setInt(1, id);
+            rs = prstm.executeQuery();
+            if (rs.next()) {
+                result = new User();
+                result.setId(rs.getInt("id"));
+                result.setLastName(rs.getString("last_name"));
+                result.setFirstName(rs.getString("first_name"));
+                result.setBirthDate(rs.getDate("birth_date"));
+                result.setEmail(rs.getString("email"));
+                result.setRole(rs.getString("role"));
+            }
+            return result;
+        } catch (Exception e) {
+            throw new AuthException("ID: " + id, e.getMessage());
+        } finally {
             Database.closeRessources(rs, prstm, c, Boolean.valueOf(isNewConnection));
         }
     }
